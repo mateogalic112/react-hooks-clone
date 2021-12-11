4ec7116e1fd44b8585b4f51471e920d1913057db
Mateo Galić <mateogalic112@gmail.com>
2021-12-11 14:11:14 +0100

    Create react setState for one state only

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-function useState(initialValue) {
-  let _val = initialValue;
-  const state = () => _val;
-  const setState = (newVal) => {
-    _val = newVal;
+const React = (function () {
+  let _val;
+  function useState(initialValue) {
+    const state = _val || initialValue;
+    const setState = (newVal) => {
+      _val = newVal;
+    };
+    return [state, setState];
+  }
+
+  function render(Component) {
+    const C = Component();
+    C.render();
+    return C;
+  }
+
+  return { useState, render };
+})();
+
+function Component() {
+  const [count, setCount] = React.useState(0);
+
+  return {
+    render: () => console.log(count),
+    click: () => setCount(count + 1),
   };
-  return [state, setState];
 }
 
-const [count, setCount] = useState(0);
-console.log(count());
-setCount(1);
-console.log(count());
+var App = React.render(Component);
+App.click();
+var App = React.render(Component);
+App.click();
+var App = React.render(Component);
