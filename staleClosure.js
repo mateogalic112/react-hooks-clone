ff0b5a953f2255f0017328f2e8e5b12286612473
Mateo Galić <mateogalic112@gmail.com>
2021-12-11 15:22:47 +0100

    Stale closure solution

--- staleClosure.js
+++ staleClosure.js
@@ -1,3 +1,4 @@
+// Problem - Stale number value in message
 let number = 0;
 
 const increment = () => {
@@ -11,6 +12,53 @@ const increment = () => {
 };
 
 const log = increment();
+log();
 increment();
 increment();
 log(); // Incremented to 1; Number: 3
+
+// Solution 1.
+
+// Move message outside so only one copy of it every exists
+/* 
+let number = 0;
+let message = "";
+
+const increment = () => {
+  number += 1;
+  message = `Incremented to ${number}`;
+
+  return () => {
+    console.log(message);
+    console.log(`Number ${number}`);
+  };
+};
+
+const log = increment();
+increment();
+increment();
+log(); 
+*/
+
+// Solution 2.
+
+// Move message into anonymous function. (Will be created on every function call) In this case: when log is called so it sees latest values
+/*
+let number = 0
+
+const increment = () => {
+    number += 1
+
+    return () => {
+        const message = `Increment to ${number}`
+
+        console.log(message)
+        console.log(`Number: ${number}`)
+    }
+}
+
+const log = increment();
+increment()
+increment()
+log()
+*/
